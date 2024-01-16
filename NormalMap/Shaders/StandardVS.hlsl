eb4e2060d165df6f257ef34b2999990add11885c
podo3501 <nixie77@naver.com>
2024-01-17 03:04:19 +0900

    normal map 프로젝트 작성

---
+++ NormalMap/Shaders/StandardVS.hlsl
@@ -0,0 +1,18 @@
+#include "GraphicsHeader.hlsli"
+#include "VertexInout.hlsli"
+
+VertexOut main( VertexIn vin )
+{
+    VertexOut vout;
+    
+    float4 posW = mul(float4(vin.PosL, 1.0f), gWorld);
+    vout.PosW = posW.xyz;
+    vout.PosH = mul(posW, gViewProj);
+    vout.NormalW = mul(vin.NormalL, (float3x3)gWorld);
+    
+    MaterialData matData = gMaterialData[gMaterialIndex];
+    float4 texC = mul(float4(vin.TexC, 0.0f, 1.0f), gTexTransform);
+    vout.TexC = mul(texC, matData.MatTransform).xy;
+	
+	return vout;
+}
